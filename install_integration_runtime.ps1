5fa132bbcead6b28084d4d3b9b7e37b261aa9913
Chad Daud <32781655+chad-38@users.noreply.github.com>
2023-09-15 16:06:21 +0100

    Update install_integration_runtime.ps1
    adding WinRM Service too

--- install_integration_runtime.ps1
+++ install_integration_runtime.ps1
@@ -1,5 +1,6 @@
 Add-WindowsCapability -Online -Name OpenSSH.Server
 Set-Service -Name sshd -StartupType 'Automatic'
 Start-Service sshd
+Enable-PSRemoting -Force
 Invoke-WebRequest -Uri 'https://download.microsoft.com/download/E/4/7/E4771905-1079-445B-8BF9-8A1A075D8A10/IntegrationRuntime_5.33.8627.2.msi' -OutFile 'C:\Temp\IntegrationRuntime_5.33.8627.2.msi'
 Start-Process -FilePath 'msiexec.exe' -ArgumentList '/i C:\Temp\IntegrationRuntime_5.33.8627.2.msi /passive'
