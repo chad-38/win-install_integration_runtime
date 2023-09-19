08f1fc197e57f7c3144d0e973fa143d17edc7867
Chad Daud <32781655+chad-38@users.noreply.github.com>
2023-09-19 12:03:41 +0100

    Create air-servers.ps1
    air-servers.ps1 included

---
+++ air-servers.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri 'https://download.microsoft.com/download/E/4/7/E4771905-1079-445B-8BF9-8A1A075D8A10/IntegrationRuntime_5.33.8627.2.msi' -OutFile 'C:\Temp\IntegrationRuntime_5.33.8627.2.msi'
+Start-Process -FilePath 'msiexec.exe' -ArgumentList '/i C:\Temp\IntegrationRuntime_5.33.8627.2.msi /passive'
